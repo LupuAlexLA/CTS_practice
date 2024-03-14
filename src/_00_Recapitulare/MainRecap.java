7bba1f76e498fefe22d833de5488afe1ecdc5c08
LupuAlexLA <alexpaullupu@gmail.com>
2024-03-14 17:10:26 +0200

    commit from mac

--- src/_00_Recapitulare/MainRecap.java
+++ src/_00_Recapitulare/MainRecap.java
@@ -3,5 +3,6 @@ package _00_Recapitulare;
 public class MainRecap {
     public static void main(String[] args) {
         System.out.println("Hello!");
+        System.out.println("Hello hello!");
     }
 }
